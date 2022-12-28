45453940f47823e27b95b0d10b26b662e3c86fdd
brayanduranvelasquez <brayan13s133@gmail.com>
2022-12-28 13:47:47 -0400

    feat: resize background and purple background

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,15 @@
+import { Stack } from '@mui/material';
+
 import { Footer } from './components/Footer';
 import { NewsSection } from './components/NewsSection';
 import { Welcome } from './components/Welcome';
 
 export default function HomePage() {
   return (
-    <>
+    <Stack bgcolor="#1F172B">
       <Welcome />
       <NewsSection />
       <Footer />
-    </>
+    </Stack>
   );
 }
